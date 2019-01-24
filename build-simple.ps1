917ea4f7aac4ffbfe4127f35f90219fb7fb6a174
Vincent Bourdon <bourdv-ext@corp.shift-technology.com>
2019-01-24 18:09:34 +0100

    build simple

---
+++ build-simple.ps1
@@ -0,0 +1,22 @@
+
+
+
+# SONAR BEGIN
+dotnet tool install --global dotnet-sonarscanner
+dotnet sonarscanner begin /k:"evilz_resharper-to-sonarqube-example" /d:sonar.organization="evilz-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="e7dcfc9c655f3ac6a72907131c093140882efa96" /d:sonar.externalIssuesReportPaths="sonarqube-report.json"
+
+
+# BUILD
+dotnet build "./src/Demo.Web.sln"
+
+# RESHARPER
+Invoke-WebRequest -Uri "https://download.jetbrains.com/resharper/ReSharperUltimate.2018.3.1/JetBrains.ReSharper.CommandLineTools.2018.3.1.zip" -OutFile ".\tools\JetBrains.ReSharper.CommandLineTools.2018.3.1.zip"
+Expand-Archive -LiteralPath .\tools\JetBrains.ReSharper.CommandLineTools.2018.3.1.zip -DestinationPath .\tools\JetBrains.ReSharper.CommandLineTools
+.\tools\JetBrains.ReSharper.CommandLineTools\InspectCode.exe "./src/Demo.Web.sln" -o="resharper-report.xml" -s=INFO
+
+# REQUBE
+dotnet tool install dotnet-reqube --tool-path tools
+.\tools\dotnet-reqube -i "resharper-report.xml" -o "sonarqube-report.json" -d "./src/"
+
+#SONAR END
+dotnet sonarscanner end /d:sonar.login="e7dcfc9c655f3ac6a72907131c093140882efa96"
